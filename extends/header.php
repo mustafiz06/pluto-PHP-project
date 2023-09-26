8bdc44a1f3d9efefe74589ce098fb4922c4d6f35
mustafiz06 <themustafiz06@gmail.com>
2023-09-26 18:03:52 +0600

    replace title name neptune ... to pluto dashboard

--- extends/header.php
+++ extends/header.php
@@ -27,13 +27,13 @@ $data = mysqli_fetch_assoc($result);
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="Responsive Admin Dashboard Template">
+    <meta name="description" content="Responsive Admin Dashboard">
     <meta name="keywords" content="admin,dashboard">
     <meta name="author" content="stacks">
     <!-- The above 6 meta tags *must* come first in the head; any other head content must come *after* these tags -->
 
     <!-- Title -->
-    <title>Neptune - Responsive Admin Dashboard Template</title>
+    <title>Pluto Dashboard</title>
 
     <!-- Styles -->
     <link rel="preconnect" href="https://fonts.gstatic.com">
