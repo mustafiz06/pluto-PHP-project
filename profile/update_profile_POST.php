8c482c54ca08f8fa1410f8932cf26752ccec0efe
mustafiz06 <themustafiz06@gmail.com>
2023-09-29 02:12:46 +0600

    update name

--- profile/update_profile_POST.php
+++ profile/update_profile_POST.php
@@ -18,7 +18,7 @@ if(isset($_POST['name_update'])){
         $name_update_query = "UPDATE users SET name='$name',address='$address' ,phone='$phone' ,profession='$profession' ,gender='$gender' ,birth_date='$birth_date',country='$country' WHERE id='$user_id'";
         mysqli_query($db_connect,$name_update_query);
 
-        $_SESSION['user_name'] = "Successfully update";
+        $_SESSION['name_update success'] = "Successfully update";
         header('location: ../home/home.php');
         
 
