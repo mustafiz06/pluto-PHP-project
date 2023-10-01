39438bb5c8a8f4f6814416ba44db8fff4c7efced
mustafiz06 <themustafiz06@gmail.com>
2023-10-02 01:53:04 +0600

    problem solve for message delete problem

--- messagebox/messagebox_POST.php
+++ messagebox/messagebox_POST.php
@@ -22,48 +22,50 @@ include('../src/SMTP.php');
 include('../src/Exception.php');
 
 
-$identify_id = $_POST['identify_id'];
-$name = $_POST['name'];
-$client_email = $_POST['client_email'];
-$answer = $db_connect->real_escape_string($_POST['answer']);
-$answer_by = $_SESSION['user_name'];
+if (isset($_GET['feedback_id'])) {
+    $identify_id = $_POST['identify_id'];
+    $name = $_POST['name'];
+    $client_email = $_POST['client_email'];
+    $answer = $db_connect->real_escape_string($_POST['answer']);
+    $answer_by = $_SESSION['user_name'];
 
-$is_sent_mail = false;
+    $is_sent_mail = false;
 
-if ($name && $client_email) {
+    if ($name && $client_email) {
 
-    $mail = new PHPMailer(true);
+        $mail = new PHPMailer(true);
 
-    // $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      
-    $mail->isSMTP();
-    $mail->Host       = 'smtp.gmail.com';
-    $mail->SMTPAuth   = true;
-    $mail->Username   = 'themustafiz06@gmail.com';
-    $mail->Password   = 'jkgc ubzy kawi pmgd';
-    $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;
-    $mail->Port       = 465;
-    //Recipients
-    $mail->setFrom('admin@pluto.com', 'Pluto Family');
-    $mail->addAddress($client_email);     //Add a recipient
-    $mail->addReplyTo($client_email);
+        // $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      
+        $mail->isSMTP();
+        $mail->Host       = 'smtp.gmail.com';
+        $mail->SMTPAuth   = true;
+        $mail->Username   = 'themustafiz06@gmail.com';
+        $mail->Password   = 'jkgc ubzy kawi pmgd';
+        $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;
+        $mail->Port       = 465;
+        //Recipients
+        $mail->setFrom('admin@pluto.com', 'Pluto Family');
+        $mail->addAddress($client_email);     //Add a recipient
+        $mail->addReplyTo($client_email);
 
-    //Content
-    $mail->isHTML(true);
-    $mail->Subject = "Reply from Pluto Family";
-    $mail->Body    = $answer;
-    $mail->AltBody = $answer;
+        //Content
+        $mail->isHTML(true);
+        $mail->Subject = "Reply from Pluto Family";
+        $mail->Body    = $answer;
+        $mail->AltBody = $answer;
 
-    $mail->send();
-    $is_sent_mail = true;
-}
-// save data after feedback reply
-if ($is_sent_mail === true) {
-    $update_query = "UPDATE messagebox SET answer='$answer', answer_by='$answer_by' WHERE id='$identify_id'";
-    mysqli_query($db_connect, $update_query);
+        $mail->send();
+        $is_sent_mail = true;
+    }
+    // save data after feedback reply
+    if ($is_sent_mail === true) {
+        $update_query = "UPDATE messagebox SET answer='$answer', answer_by='$answer_by' WHERE id='$identify_id'";
+        mysqli_query($db_connect, $update_query);
 
-    $_SESSION['add success'] = 'successfully sent';
-    header('location:./messagebox.php');
-} else {
-    $_SESSION['add error'] = 'Failed to send';
-    header('location:./feedback.php');
+        $_SESSION['add success'] = 'successfully sent';
+        header('location:./messagebox.php');
+    } else {
+        $_SESSION['add error'] = 'Failed to send';
+        header('location:./feedback.php');
+    }
 }
